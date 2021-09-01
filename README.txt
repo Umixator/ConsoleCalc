bfe9385b53b8b0554e6dd54ce886b848b123c1b7
Верещак Артем <umixator@gmail.com>
2021-09-01 19:32:43 +0300

    Добавьте файлы проекта.

---
+++ README.txt
@@ -0,0 +1,27 @@
+How to use Calculator:
+
+1. Enter a calculation in to the input line by your keyboard.
+You can only put 2 arguments in to calculation and do one of operations:
++
+-
+*
+/
+Also you can delete your input by pressing "Backspace" on your keyboard,
+or write C and press "Enter" to clear the whole console.
+
+2. Press "Enter" key on your keyboard to see the result.
+Your result will be displayed on the same input line and you can continue make your calculations.
+
+
+
+Program structure:
+
+Application is based on monolithic architecture.
+Development pattern wasn't applied to development.
+Program - the only class for the 
+
+Program contains next methods:
+
+Calculation - returns calculation between two variables;
+TakeArgs - returns result in to user's input.
+Run - makes cycle for console application to continue working.
---
+++ RemakeSolbCalc/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+
+namespace RemakeSolbCalc
+{
+    class Program
+    {
+        static readonly char[] operList = { '+', '-', '*', '/' };
+        static string result = "";
+
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static string Calculation(double A, double B, char oper) //method for calculations
+        {
+            {
+
+            }
+            switch (oper)
+            {
+                case '+':
+                    return Convert.ToString(A + B);
+                case '-':
+                    return Convert.ToString(A - B);
+                case '*':
+                    return Convert.ToString(A * B);
+                case '/':
+
+                    if (B != 0)
+                    {
+                        return Convert.ToString(A / B);
+                    }
+                    else
+                    {                  
+                        Console.WriteLine("Can't divide by 0, try new calculation");
+                        return "0";
+                    }                  
+
+                default: return "";
+            }
+        }
+
+        static string TakeArgs() //method for assigning values to arguments
+        {
+            string input = Console.ReadLine();
+
+            if (input == "C" || input == "С")
+            {
+                Console.Clear();
+                return result = "0";
+            }
+
+            else if (!char.IsDigit(input[0]))
+            {
+                double[] arg = input.Split(operList, StringSplitOptions.RemoveEmptyEntries).Select(n => Convert.ToDouble(n)).ToArray();
+                char oper = operList.FirstOrDefault(input.Contains);
+                result = Calculation(Convert.ToDouble(result), arg[0], oper);
+                return result;
+            }
+
+            else
+            {
+                double[] argList = input
+                    .Split(operList, StringSplitOptions
+                    .RemoveEmptyEntries)
+                    .Select(n => Convert.ToDouble(n)).ToArray();  // parsing user input in 2 arguments
+
+                char oper = operList.FirstOrDefault(input.Contains);
+                result = argList[0].ToString();
+                result = Calculation(Convert.ToDouble(result), argList[1], oper);
+                return result;
+            }
+        }
+
+        static void Run() //method that runs a cycle of the programm
+        {
+            do
+            {
+                Console.WriteLine(TakeArgs());
+            }
+            while (true);
+        }
+    }
+}
